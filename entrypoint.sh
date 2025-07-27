b57317aeca2a55f42beac3cca204031a0705b87b
vasu kommi <itsvasukommi@gmail.com>
2025-07-27 10:59:28 +0530

    wait for postgres before migrations

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,30 @@
 #!/bin/sh
 set -e
 
+# Wait for PostgreSQL to become available before running migrations
+python - <<'PY'
+import os
+import time
+from sqlalchemy import create_engine
+from sqlalchemy.exc import OperationalError
+
+db_url = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:postgres@postgres:5432/postgres",
+)
+
+engine = create_engine(db_url)
+print("Waiting for database...", flush=True)
+while True:
+    try:
+        with engine.connect():
+            break
+    except OperationalError:
+        time.sleep(1)
+
+print("Database is ready", flush=True)
+PY
+
 # Run database migrations
 alembic upgrade head
 
